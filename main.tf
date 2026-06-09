9cca1e5633699e39733a67cef2aaf78bb7407cf1
Gaurvssris <gauravsrivastwa85@gmail.com>
2026-06-10 01:20:35 +0530

    newwalarg

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.57.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "84dd7c9c-ad3c-4682-abd1-5403bd6ffa0d"
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "gauravs" {
+  name     = "gauravss"
+  location = "East US"
+}
